2e965601eb5b28efcafc3bbea8de22180d29aa5f
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-07-14 15:58:00 -0400

    lab done

--- index.js
+++ index.js
@@ -17,11 +17,51 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    //return Recipe.deleteMany();
+      return self.connection.dropDatabase();
   })
-  .then(() => {
+  .then(() => {  //ITERATION 2
     // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    Recipe.create({title: 'pizza', cuisine:'italian'})
+    .then(recipe => console.log(`The recipe:${recipe.title} is saved`))
+    .catch(error => console.log(`An error happened while saving recipe.`, error))
+     //ITERATION 3
+    Recipe.insertMany(data)
+    .then(data => data.forEach(recipe => console.log(recipe.title)))
+    .catch(error=>console.log('An error happened while saving recipe.', error))
+    //ITERATION 4
+    setTimeout(() => {
+      Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true },
+      (error, data) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Successfully updated', data);
+        }
+      }
+    );
+    
+  }, 2000)
+ //ITERATION 5
+  setTimeout(() => {
+    Recipe.deleteOne({title:'Carrot Cake'})
+    .then( 
+      console.log(`Successfully deleted`)
+    )
+    .catch(error => {
+      console.error('Error while deleting', error)
+    });
+  }, 2500)
+  
+    })
+    .catch(error => {
+      console.error('Error connecting to the database', error);
+    });
+
+//Iteration 6
+    process.on("SIGINT", () => {
+      mongoose.connection.close(() => {
+        console.log("Connection disconnected");
+        process.exit(0);
+      })
+    })
